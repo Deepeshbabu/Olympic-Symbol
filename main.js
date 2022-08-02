5ff240a76647aadeff065ff004fada496a3ca70b
Deepeshbabu <104300644+Deepeshbabu@users.noreply.github.com>
2022-08-02 21:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var can = document.getElementById("myCanvas") ;
+
+var ctx = can.getContext("2d") ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "black" ;
+ctx.lineWidth = 1 ;
+ctx.rect(150,143,430,200) ;
+ctx.stroke() ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "blue" ;
+ctx.lineWidth = 5 ;
+ctx.arc(260,220,40,0,2*Math.PI) ;
+ctx.stroke() ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "black" ;
+ctx.lineWidth = 5 ;
+ctx.arc(360,220,40,0,2*Math.PI) ;
+ctx.stroke() ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "red" ;
+ctx.lineWidth = 5 ;
+ctx.arc(460,220,40,0,2*Math.PI) ;
+ctx.stroke() ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "yellow" ;
+ctx.lineWidth = 5 ;
+ctx.arc(310,270,40,0,2*Math.PI) ;
+ctx.stroke() ;
+
+ctx.beginPath() ;
+ctx.strokeStyle = "green" ;
+ctx.lineWidth = 5 ;
+ctx.arc(410,270,40,0,2*Math.PI) ;
+ctx.stroke() ;
